84a73afd620bf2793b9b7cdcebe8a1c2133b0c17
Alma Flores <flores.almalee@gmail.com>
2020-08-31 18:01:37 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def songs 
+    self.songs
+  end
+  
+  def add_song(song)
+  end 
+
+  def add_song_by_name(song_name)
+  end 
+
+  def song_count
+  end
+end
